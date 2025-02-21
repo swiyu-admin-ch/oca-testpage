9c587650a87e398e49084c5e498976fdfc62241a
Pavel Zarecky <pavel.zarecky@bit.admin.ch>
2025-02-21 17:43:17 +0100

    Refactoring detail renderer

--- src/app/renderer/vc-detail/vc-detail.component.ts
+++ src/app/renderer/vc-detail/vc-detail.component.ts
@@ -41,8 +41,7 @@ export class VcDetailComponent {
     }
 
     if (clusterOrder) {
-      this.parseClusterOrder(
-        this.vcDisplay,
+      this.vcDisplay = this.parseClusterOrder(
         captureBase,
         mappedValues,
         clusterOrder,
@@ -53,28 +52,29 @@ export class VcDetailComponent {
   }
 
   parseClusterOrder(
-    vcDisplay: Array<{ type: string; value: string }>,
     captureBase: CaptureBase,
     values: any,
     clusterOrder: ClusterOrderingOverlay,
     standard: StandardOverlay | undefined,
     labels: LabelOverlay | undefined
-  ) {
-    const clusterOrdered = Object.keys(clusterOrder['cluster_order']).sort(
-      (a, b) => clusterOrder['cluster_order'][a] - clusterOrder['cluster_order'][b]
+  ): Array<{ type: string; value: string }> {
+    const clusterOrdered = Object.keys(clusterOrder.cluster_order).sort(
+      (a, b) => clusterOrder.cluster_order[a] - clusterOrder.cluster_order[b]
     );
+
+    const result = [];
 
     for (const clusterKey in clusterOrdered) {
       const clusterValue = clusterOrdered[clusterKey];
       if (clusterOrder.cluster_labels?.[clusterValue]) {
-        vcDisplay.push({ type: 'title', value: clusterOrder.cluster_labels[clusterValue] });
+        result.push({ type: 'title', value: clusterOrder.cluster_labels[clusterValue] });
       }
       const attributesOrdered = Object.keys(
-        clusterOrder['attribute_cluster_order'][clusterValue]
+        clusterOrder.attribute_cluster_order[clusterValue]
       ).sort(
         (a, b) =>
-          clusterOrder['attribute_cluster_order'][clusterValue][a] -
-          clusterOrder['attribute_cluster_order'][clusterValue][b]
+          clusterOrder.attribute_cluster_order[clusterValue][a] -
+          clusterOrder.attribute_cluster_order[clusterValue][b]
       );
       //TOOD add support for data arrays!
 
@@ -84,32 +84,32 @@ export class VcDetailComponent {
       for (let i = 0; i < values.length; i++) {
         for (const attributeKey in attributesOrdered) {
           const attributeValue = attributesOrdered[attributeKey];
-          if (labels?.['attribute_labels'][attributeValue]) {
-            vcDisplay.push({ type: 'label', value: labels['attribute_labels'][attributeValue] });
+          if (labels?.attribute_labels[attributeValue]) {
+            result.push({ type: 'label', value: labels.attribute_labels[attributeValue] });
           }
 
-          const attributeType = captureBase['attributes'][attributeValue];
+          const attributeType = captureBase.attributes[attributeValue];
           if (attributeType === 'Number') {
-            vcDisplay.push({ type: 'text', value: values[i][attributeValue] });
+            result.push({ type: 'text', value: values[i][attributeValue] });
           } else if (attributeType === 'Text') {
-            if (standard && standard['attr_standards'][attributeValue] === 'urn:ietf:rfc:2397') {
-              vcDisplay.push({ type: 'image', value: values[i][attributeValue] });
+            if (standard && standard.attr_standards[attributeValue] === 'urn:ietf:rfc:2397') {
+              result.push({ type: 'image', value: values[i][attributeValue] });
             } else {
-              vcDisplay.push({ type: 'text', value: values[i][attributeValue] });
+              result.push({ type: 'text', value: values[i][attributeValue] });
             }
           } else if (attributeType === 'DateTime') {
             const timestamp = Date.parse(values[i][attributeValue]);
             if (!isNaN(timestamp)) {
               const date = new Date(timestamp);
-              vcDisplay.push({
+              result.push({
                 type: 'text',
                 value: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
               });
             }
           } else if (attributeType.startsWith('refs:') || attributeType.startsWith('Array[refs:')) {
             const refDigest = attributeType.startsWith('refs:')
-              ? captureBase['attributes'][attributeValue].slice(5, -1)
-              : captureBase['attributes'][attributeValue].slice(11, -1);
+              ? captureBase.attributes[attributeValue].slice(5, -1)
+              : captureBase.attributes[attributeValue].slice(11, -1);
 
             const refCaptureBase = this.ocaService.getCaptureBaseByDigest(this.oca, refDigest);
             const refClusterOrder = this.ocaService.getOverlayByDigest(
@@ -133,22 +133,25 @@ export class VcDetailComponent {
             const refMappedValue = values[i][attributeValue];
 
             if (refClusterOrder) {
-              this.parseClusterOrder(
-                vcDisplay,
-                refCaptureBase,
-                refMappedValue,
-                refClusterOrder,
-                refStandard,
-                refLabels
+              result.push(
+                ...this.parseClusterOrder(
+                  refCaptureBase,
+                  refMappedValue,
+                  refClusterOrder,
+                  refStandard,
+                  refLabels
+                )
               );
             }
           }
         }
 
         if (i != values.length - 1) {
-          vcDisplay.push({ type: 'divider', value: '' });
+          result.push({ type: 'divider', value: '' });
         }
       }
     }
+
+    return result;
   }
 }
