3e3ef23877624e128a0cb2b4c47846fa772511ae
Pavel Zarecky <pavel.zarecky@bit.admin.ch>
2025-03-26 11:37:50 +0100

    Additional overlay validation

--- src/app/validator/overlay.ts
+++ src/app/validator/overlay.ts
@@ -2,9 +2,12 @@ import {
   AnyOverlay,
   BrandingOverlay1_1,
   CaptureBase,
+  ClusterOrderingOverlay,
   DataSourceOverlay,
+  FormatOverlay,
   LabelOverlay,
-  OverlaySpecType
+  OverlaySpecType,
+  StandardOverlay
 } from '../model';
 
 export class OCAOverlayError extends Error {
@@ -76,6 +79,74 @@ function validateLabel(overlay: LabelOverlay, relatedBase: CaptureBase) {
     });
 }
 
+function validateFormat(overlay: FormatOverlay, relatedBase: CaptureBase) {
+  Object.keys(overlay.attribute_formats).forEach((attribute) => {
+    if (!(attribute in relatedBase.attributes)) {
+      throw new OCAOverlayError(
+        `attribute_formats/${attribute}`,
+        `Attribute "${attribute}" not found in related capture base attributes`
+      );
+    }
+  });
+}
+
+function validateStandard(overlay: StandardOverlay, relatedBase: CaptureBase) {
+  Object.keys(overlay.attr_standards).forEach((attribute) => {
+    if (!(attribute in relatedBase.attributes)) {
+      throw new OCAOverlayError(
+        `attr_standards/${attribute}`,
+        `Attribute "${attribute}" not found in related capture base attributes`
+      );
+    }
+  });
+}
+
+function validateClusterOrdering(overlay: ClusterOrderingOverlay, relatedBase: CaptureBase) {
+  const clusterOrderKeys = Object.keys(overlay.cluster_order);
+
+  if (new Set(Object.values(overlay.cluster_order)).size !== clusterOrderKeys.length) {
+    throw new OCAOverlayError(`cluster_order`, `Cluster order values must be unique`);
+  }
+
+  if (overlay.cluster_labels) {
+    Object.keys(overlay.cluster_labels).forEach((key) => {
+      if (!clusterOrderKeys.includes(key)) {
+        throw new OCAOverlayError(
+          `cluster_labels/${key}`,
+          `Key "${key}" not found in cluster_order`
+        );
+      }
+    });
+  }
+
+  Object.keys(overlay.attribute_cluster_order).forEach((key) => {
+    if (!clusterOrderKeys.includes(key)) {
+      throw new OCAOverlayError(
+        `attribute_cluster_order/${key}`,
+        `Key "${key}" not found in cluster_order`
+      );
+    }
+  });
+
+  Object.entries(overlay.attribute_cluster_order).forEach(([clusterKey, clusterAttributes]) => {
+    Object.keys(clusterAttributes).forEach((attribute) => {
+      if (!(attribute in relatedBase.attributes)) {
+        throw new OCAOverlayError(
+          `attribute_cluster_order/${clusterKey}/${attribute}`,
+          `Attribute "${attribute}" not found in related capture base attributes`
+        );
+      }
+    });
+
+    if (new Set(Object.values(clusterAttributes)).size !== Object.keys(clusterAttributes).length) {
+      throw new OCAOverlayError(
+        `attribute_cluster_order/${clusterKey}`,
+        `Cluster attribute order values must be unique`
+      );
+    }
+  });
+}
+
 export function validateOverlay(overlay: AnyOverlay, relatedBase: CaptureBase) {
   switch (overlay.type) {
     case OverlaySpecType.BRANDING_1_1:
@@ -86,6 +157,15 @@ export function validateOverlay(overlay: AnyOverlay, relatedBase: CaptureBase) {
       break;
     case OverlaySpecType.LABEL_1_0:
       validateLabel(overlay, relatedBase);
+      break;
+    case OverlaySpecType.FORMAT_1_0:
+      validateFormat(overlay, relatedBase);
+      break;
+    case OverlaySpecType.STANDARD_1_0:
+      validateStandard(overlay, relatedBase);
+      break;
+    case OverlaySpecType.CLUSTER_ORDERING_1_0:
+      validateClusterOrdering(overlay, relatedBase);
       break;
   }
 }
